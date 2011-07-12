8553385c4b0d99b61422e803672ba5932c616a5b
Andre Joseph Cubeta <tech3@jobline.com.sg>
2011-07-12 15:07:02 +0800

    Commit 1st batch files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddDemo::Application.config.secret_token = '6b9ee963d4ca1c44790badb0fe3ac194d049593a7833086625cb736b40a011c27cfe04f91b35a0e5947e2d13082ed8f16b97716813ef0c938c237e20ec4f8a4c'
